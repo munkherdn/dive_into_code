b06af164a3407f8db696553f45e3eaa66b6b374d
munkherdn <munkherdn@gmail.com>
2023-11-22 16:24:33 +0800

    sorochi

--- Sorochi.py
+++ Sorochi.py
@@ -1,19 +1,12 @@
 import matplotlib.pyplot as plt
 
 def compute_sorori_shinzaemon(n_days=100):
-
-    list_n_grains = []  # List to store the number of grains received each day
-    list_total_grains = []  # List to store the total number of grains received by a certain day
-    total_grains = 0  # Variable to keep track of the total number of grains
-
-    for day in range(1, n_days + 1):
-        grains_on_current_day = 2 ** (day - 1)
-        total_grains += grains_on_current_day
-
-        list_n_grains.append(grains_on_current_day)
-        list_total_grains.append(total_grains)
-
+    list_n_grains = [2**(day - 1) for day in range(1, n_days + 1)]
+    list_total_grains = [sum(list_n_grains[:i + 1]) for i in range(n_days)]
     return list_n_grains, list_total_grains
+
+def calculate_days_of_survival(total_grains, num_people, grains_per_day_per_person):
+    return total_grains // (num_people * grains_per_day_per_person)
 
 # Problem 1
 list_n_grains_1, list_total_grains_1 = compute_sorori_shinzaemon(n_days=100)
@@ -26,6 +19,12 @@ plt.plot(range(1, 101), list_n_grains_1, label='Grains on the nth day (Problem 1
 plt.plot(range(1, 101), list_total_grains_1, label='Total grains by nth day (Problem 1)')
 plt.plot(range(1, 11), list_n_grains_2, label='Grains on the nth day (Problem 2)')
 plt.plot(range(1, 11), list_total_grains_2, label='Total grains by nth day (Problem 2)')
+
+# Problem 3
+total_grains_3 = list_total_grains_1[-1]
+num_people_3, grains_per_day_per_person_3 = 5, 400
+days_of_survival = calculate_days_of_survival(total_grains_3, num_people_3, grains_per_day_per_person_3)
+print(f"With {total_grains_3} grains, {num_people_3} people can survive for {days_of_survival} days.")
 
 plt.xlabel('Number of days')
 plt.ylabel('Number of rice grains')
