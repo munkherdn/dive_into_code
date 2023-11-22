b0889677a75a82dfbd341b93ccb99623b4e80f19
munkherdn <munkherdn@gmail.com>
2023-11-22 16:21:11 +0800

    sorochi

--- Sorochi.py
+++ Sorochi.py
@@ -1,28 +1,34 @@
 import matplotlib.pyplot as plt
 
-def calculate_rice_grains(day):
-    return 2 ** (day - 1)
+def compute_sorori_shinzaemon(n_days=100):
 
-def calculate_total_rice_grains(day):
-    total_grains = sum(calculate_rice_grains(i) for i in range(1, day + 1))
-    return total_grains
+    list_n_grains = []  # List to store the number of grains received each day
+    list_total_grains = []  # List to store the total number of grains received by a certain day
+    total_grains = 0  # Variable to keep track of the total number of grains
 
-# Calculate the number of grains on each day and the total grains
-days = list(range(1, 101))
-rice_per_day = [calculate_rice_grains(day) for day in days]
-total_rice_per_day = [calculate_total_rice_grains(day) for day in days]
+    for day in range(1, n_days + 1):
+        grains_on_current_day = 2 ** (day - 1)
+        total_grains += grains_on_current_day
 
-# Plotting the results
-plt.figure(figsize=(10, 6))
-plt.plot(days, rice_per_day, label='Rice grains on the day', marker='o')
-plt.plot(days, total_rice_per_day, label='Total rice grains by the day', marker='o')
-plt.xlabel('Days')
-plt.ylabel('Number of Rice Grains')
-plt.title('Change in Number of Rice Grains Over 100 Days')
+        list_n_grains.append(grains_on_current_day)
+        list_total_grains.append(total_grains)
+
+    return list_n_grains, list_total_grains
+
+# Problem 1
+list_n_grains_1, list_total_grains_1 = compute_sorori_shinzaemon(n_days=100)
+
+# Problem 2 (Example: 10 days)
+list_n_grains_2, list_total_grains_2 = compute_sorori_shinzaemon(n_days=10)
+
+# Plotting
+plt.plot(range(1, 101), list_n_grains_1, label='Grains on the nth day (Problem 1)')
+plt.plot(range(1, 101), list_total_grains_1, label='Total grains by nth day (Problem 1)')
+plt.plot(range(1, 11), list_n_grains_2, label='Grains on the nth day (Problem 2)')
+plt.plot(range(1, 11), list_total_grains_2, label='Total grains by nth day (Problem 2)')
+
+plt.xlabel('Number of days')
+plt.ylabel('Number of rice grains')
+plt.title('Number of Rice Grains over Time')
 plt.legend()
-plt.grid(True)
 plt.show()
-
-# Output the total number of rice grains on the 100th day
-day_100_total_rice = calculate_total_rice_grains(100)
-print(f'Total number of rice grains on the 100th day: {day_100_total_rice}')
